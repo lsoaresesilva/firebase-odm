e1262f64a3ec5e75782f2883f3d4cb020d27f0da
lsoaresesilva <mac@Mac-mini-de-Mac.local>
2018-07-17 22:33:44 -0300

    atualizando diretórios

--- README.md
+++
@@ -1,3 +0,0 @@
-# Firebase Object Document Mapper
-
-This project is in early stage. Its objective is to provide a document to javascript object mapper, through Firestore database.
--- src/app/entity/test-config.ts
+++
@@ -1,8 +0,0 @@
-export const COMMON_CONFIG = {
-    apiKey: "AIzaSyAwyq96fa1dKRKoTo-6q9KGVe8Xv0rPq4I",
-    authDomain: "app-fidelidade-559ea.firebaseapp.com",
-    databaseURL: "https://app-fidelidade-559ea.firebaseio.com",
-    projectId: "app-fidelidade-559ea",
-    storageBucket: "",
-    messagingSenderId: "616881159089"
-}
